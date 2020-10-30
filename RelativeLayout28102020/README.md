395420202dc075f787312eda920370ad479754ca
zarack06 <nguyentienanh199@gmail.com>
2020-10-30 14:24:39 +0700

    Nguyen Anh Tien

---
+++ RelativeLayout28102020/README.md
@@ -0,0 +1,1 @@
+"# RealtiveLayout2810_Tien" 
---
+++ RelativeLayout28102020/app/src/main/java/com/example/relativelayout05102020/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.relativelayout05102020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
